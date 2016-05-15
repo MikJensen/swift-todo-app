cc3fb275da03182f4f1e87d9f7536fea0d4276fb
Jógvan Olsen <jo@jebster.net>
2016-05-15 14:11:14 +0200

    Added user info to settings tab

--- swift-todo-app/SettingsViewController.swift
+++ swift-todo-app/SettingsViewController.swift
@@ -8,28 +8,57 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tabBar: TabBarViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+        tabBar = self.tabBarController as! TabBarViewController
 
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Bruger info"
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+
+        switch(indexPath.row){
+            case 0:
+                cell.textLabel?.text = "Brugernavn"
+                cell.detailTextLabel!.text = tabBar.userModel.user?.username
+            case 1:
+                cell.textLabel?.text = "Fuldenavn"
+                cell.detailTextLabel!.text = tabBar.userModel.user?.fullname
+            case 2:
+                cell.textLabel?.text = "År"
+                cell.detailTextLabel!.text = "\(tabBar.userModel.user!.age)"
+            default:()
+        }
+
+        return cell
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
